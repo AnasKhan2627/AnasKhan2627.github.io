91f45880bcba67769e3d65474e1e248c985a988b
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-06-20 08:05:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var defferedPrompt;
 if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('../sw.js').then(function(){
+    navigator.serviceWorker.register('sw.js').then(function(){
 console.log("sw registered");
     });
+}
 window.addEventListener('beforeinstallprompt',function(event){
 console.log('beforeinstallprompt fired');
 event.preventDefault();
